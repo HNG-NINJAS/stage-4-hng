1340f5af1c9007ab2852a97e3781b073489eaf30
Steel Maxima <steelmaxima21@gmail.com>
2025-11-10 23:05:03 +0100

    feat:add email-conumer service

---
+++ src/modules/email/email-consumer.service.ts
@@ -0,0 +1,106 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as amqp from 'amqplib';
+import { EmailService } from './email.service';
+interface EmailQueueMessage {
+  notification_id: string;
+  user_id: string;
+  type: 'email';
+  template_id: string;
+  variables: Record<string, string>;
+  priority: 'high' | 'normal' | 'low';
+  timestamp: string;
+  metadata?: any;
+}
+@Injectable()
+export class EmailConsumerService implements OnModuleInit {
+  private logger = new Logger(EmailConsumerService.name);
+  private connection: amqp.Connection;
+  private channel: amqp.Channel;
+  constructor(
+    private configService: ConfigService,
+    private emailService: EmailService,
+  ) {}
+
+  async onModuleInit() {
+    await this.connectToRabbitMQ();
+    await this.startConsuming();
+  }
+  private async conectToRabbitMQ() {
+    try {
+      const url = this.configService.get<string>('rabbitmq.url');
+      this.connection = await amqp.connect(url);
+      this.channel = await this.connection.createChannel();
+      this.logger.log('Connected to RabbitMQ');
+      this.connection.on('error', (err) => {
+        this.logger.error(`RabbitMQ connection error:`, err);
+      });
+      this.connection.on('close', () => {
+        this.logger.warn('RabbitMq Connection Closed,Reconnecting....');
+        setTimeout(() => this.conectToRabbitMQ(), 5000);
+      });
+    } catch (err) {
+      this.logger.error('Failed to connect to RabbitMQ:', err);
+      setTimeout(() => this.conectToRabbitMQ(), 5000);
+    }
+  }
+  private async startConsuming() {
+    const queueName = this.configService.get<string>('rabbitmq.queue');
+    const prefetchCount = this.configService.get<number>(
+      'rabbitmq.prefetchCount',
+    );
+    // Ensure the queue exists
+    await this.channel.assertQueue(queueName, { durable: true });
+    this.channel.prefetch(prefetchCount);
+    this.logger.log(`Consume from queue:${queueName}`);
+    //consume message
+    this.channel.consume(queueName, async (msg) => {
+      if (!msg) return;
+      try {
+        const message: EmailQueueMessage = JSON.parse(msg.content.toString());
+        this.logger.log(`Recieved message:${message.notification_id}`);
+        //Process email
+        await this.emailService.processEmail(message);
+        // ACK message
+        this.channel.ack(msg);
+        this.logger.log(`Processed :${message.notification_id}`);
+      } catch (error) {
+        this.logger.error('Error processing message:', error);
+        const retryCount =
+          (msg.properties.headers['x-retry-count'] as number) || 0;
+        const maxRetries = this.configService.get<number>('retry.maxRetries')!;
+        if (retryCount < maxRetries) {
+          const delay =
+            Math.pow(2, retryCount) *
+            this.configService.get<number>('retry.delay')!;
+          this.logger.log(
+            `Retrying in ${delay}ms (attempt ${retryCount + 1}/${maxRetries})`,
+          );
+
+          setTimeout(() => {
+            this.channel.publish('', queueName, msg.content, {
+              ...msg.properties,
+              headers: {
+                ...msg.properties.headers,
+                'x-retry-count': retryCount + 1,
+              },
+            });
+            this.channel.ack(msg);
+          }, delay);
+        } else {
+          // Move to dead letter queue
+          this.logger.error(`💀 Max retries exceeded. Moving to DLQ`);
+          this.channel.nack(msg, false, false);
+        }
+      }
+    },{
+        noAck:false
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.channel?.close();
+    await this.connection?.close();
+    this.logger.log('Disconnected from RabbitMQ');
+  }
+}
